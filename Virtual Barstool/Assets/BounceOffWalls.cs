a61dc1e62b7bf7d9386e8d93b3163bfaceae7eb8
Steven <>
2018-12-02 21:49:30 -0500

    added untracked file

---
+++ Virtual Barstool/Assets/BounceOffWalls.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceOffWalls : MonoBehaviour
+{
+
+
+    private Rigidbody rb;
+    private int currentIndex;
+    public int frameSaves = 10;
+    //lower number slows down ball on wall collision
+    public float speedLoss = 1.0F;
+    public Vector3[] preHitVel;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentIndex = 0;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        preHitVel[currentIndex++] = rb.velocity;
+        if (currentIndex >= frameSaves)
+        {
+            currentIndex = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        int temp = currentIndex + 1;
+        if (temp >= frameSaves)
+        {
+            temp = 0;
+        }
+
+        if (other.GetComponent<whichwall>().LongWall)
+        {
+            rb.velocity = new Vector3(-speedLoss * preHitVel[temp].x, 0, rb.velocity.z);
+        }
+        else
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0, -speedLoss * preHitVel[temp].z);
+        }
+    }
+}
