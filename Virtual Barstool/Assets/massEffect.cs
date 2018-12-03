f9283202288a87cdb7d6611b89789d0d18549b82
Steven <>
2018-12-02 23:06:03 -0500

    modified mass of white ball to have a more realistic break

---
+++ Virtual Barstool/Assets/massEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class massEffect : MonoBehaviour {
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.transform.parent.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("COLLISION");
+        if (collision.gameObject.tag == "Balls")
+        {
+            rb.mass = 3;
+        }
+    }
+}
